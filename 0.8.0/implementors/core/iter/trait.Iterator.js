7da65c822b0f92bfc0c57aad3b5e8d54b031e0c2
Sean McArthur <sean.monstar@gmail.com>
2016-03-24 18:42:28 -0700

    build docs for 0.8.0

---
+++ 0.8.0/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hyper'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hyper/header/struct.HeadersItems.html' title='hyper::header::HeadersItems'>HeadersItems</a>&lt;'a&gt;","impl&lt;'a, N: <a class='trait' href='hyper/net/trait.NetworkListener.html' title='hyper::net::NetworkListener'>NetworkListener</a> + 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hyper/net/struct.NetworkConnections.html' title='hyper::net::NetworkConnections'>NetworkConnections</a>&lt;'a, N&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
